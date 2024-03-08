b9cefd79f9a57c4e614c49718faaf76d82678bd3
Strong-Finger-sudo <80513758+Strong-Finger-sudo@users.noreply.github.com>
2024-03-08 16:25:49 +0100

    requirements.txt

--- bot/config.py
+++ bot/config.py
@@ -7,11 +7,11 @@ API_HASH = "379589fd8763fdfa87a3bb8f78953277"
 ADMINS = [651910998, 1068622041, 876255386]
 
 #DB CONFIG
-DB_HOST = "localhost"
-DB_NAME = "postgres"
+DB_HOST = "dpg-cnkgm0ta73kc738f4qb0-a.frankfurt-postgres.render.com"
+DB_NAME = "paradise_mch8"
 DB_PORT = "5432"
-DB_PASS = "1111"
-DB_USER = "postgres"
+DB_PASS = "VV9zq5ByGYZ1ZQpcJlbwqhsg332HFTMc"
+DB_USER = "paradise_mch8_user"
 
 #PROMOTERS
 PROMOTERS = [
